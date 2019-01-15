37c3538e7f430ddf6ef8f8907cafb20a28e945b7
ta-david-yu <d366095@gmail.com>
2019-01-16 00:10:26 +0800

    Updates

---
+++ precache-manifest.1f244f0ebd43768b8ba8d6436ec993bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb9bbd4c194e1c136ea7",
+    "url": "/about-me/static/js/runtime~main.cb9bbd4c.js"
+  },
+  {
+    "revision": "857cf6fd09bf531002d2",
+    "url": "/about-me/static/js/main.857cf6fd.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/about-me/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "857cf6fd09bf531002d2",
+    "url": "/about-me/static/css/main.0cfaaa4b.chunk.css"
+  },
+  {
+    "revision": "1d1bed17b857966b58647623f40c5018",
+    "url": "/about-me/index.html"
+  }
+];
