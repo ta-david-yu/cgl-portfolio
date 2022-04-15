194c7b53021bde73e6215ecbab07d14961b5e133
ta-david-yu <d366095@gmail.com>
2022-04-15 21:20:53 +0200

    Updates

---
+++ precache-manifest.c8ad7ff2bcb83e269bead017b7db053b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "06395abf41c85035bfa146196fbb672b",
+    "url": "/cgl-portfolio/static/media/m6x11.06395abf.woff"
+  },
+  {
+    "revision": "eb9f8b0544515241e59f",
+    "url": "/cgl-portfolio/static/css/main.cf5cb5cd.chunk.css"
+  },
+  {
+    "revision": "caa2ab97a3bd95994438",
+    "url": "/cgl-portfolio/static/js/runtime~main.caa2ab97.js"
+  },
+  {
+    "revision": "7aed82757584e8ace258",
+    "url": "/cgl-portfolio/static/js/2.7aed8275.chunk.js"
+  },
+  {
+    "revision": "1681d9b97eb6ced79230094129e0c5b3",
+    "url": "/cgl-portfolio/static/media/m5x7.1681d9b9.woff2"
+  },
+  {
+    "revision": "b99dd26c57ef102981d6905fe699d79a",
+    "url": "/cgl-portfolio/static/media/m5x7.b99dd26c.woff"
+  },
+  {
+    "revision": "eb9f8b0544515241e59f",
+    "url": "/cgl-portfolio/static/js/main.eb9f8b05.chunk.js"
+  },
+  {
+    "revision": "4b67f4cae7b165eb8df707021f06dccf",
+    "url": "/cgl-portfolio/static/media/m6x11.4b67f4ca.woff2"
+  },
+  {
+    "revision": "cc8dfaa3938829493e45c4a13b6629c3",
+    "url": "/cgl-portfolio/static/media/m6x11.cc8dfaa3.ttf"
+  },
+  {
+    "revision": "ec0d4e88e6dbb03bb5ae247d39f22569",
+    "url": "/cgl-portfolio/static/media/m6x11.ec0d4e88.svg"
+  },
+  {
+    "revision": "1c36fac5bce0b7b7e40d1d6d6b1030fb",
+    "url": "/cgl-portfolio/static/media/m5x7.1c36fac5.ttf"
+  },
+  {
+    "revision": "dc97bf0051a47911c9ef8e5bef5c037f",
+    "url": "/cgl-portfolio/static/media/m5x7.dc97bf00.svg"
+  },
+  {
+    "revision": "fda47ae9f32e918a99dbe76fc21e8812",
+    "url": "/cgl-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/about-me/precache-manifest.32bc5431f9b0f160971d9efb908eacc9.js"
+  "/cgl-portfolio/precache-manifest.c8ad7ff2bcb83e269bead017b7db053b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/about-me/index.html", {
+workbox.routing.registerNavigationRoute("/cgl-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
